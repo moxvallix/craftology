8c6bac6d6bf6e0ddb86d14f1d581a9a23136c7f9
Moxvallix <moxvallix@gmail.com>
2024-02-07 03:11:23 +1030

    fix: correct format for tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './public/scripts'
+    './public/scripts/**/*.js'
   ],
   theme: {
     extend: {
