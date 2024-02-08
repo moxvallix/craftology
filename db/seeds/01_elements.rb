457904ab59bf272acce7324b7b153bcda66feb03
Moxvallix <moxvallix@gmail.com>
2024-02-08 11:54:49 +1030

    chore: fix element seed

--- db/seeds/01_elements.rb
+++ db/seeds/01_elements.rb
@@ -1,10 +1,10 @@
 ELEMENTS = "db/seeds/fixtures/elements.json".freeze
 
-JSON.parse(File.read(ELEMENTS)).each do |name, description, icon, default|
+JSON.parse(File.read(ELEMENTS)).each do |name, icon, description, default|
   element = Element.find_by_name name
   if element.present?
-    element.update(description: description, icon: icon, default: default)
+    element.update(description: description, icon: icon, default: default || false)
     next
   end
-  Element.create(name: name, description: description, icon: icon, default: default)
+  Element.create(name: name, description: description, icon: icon, default: default || false)
 end
