208ab533940d287e627b9160a1f2e07803669f2b
Moxvallix <moxvallix@gmail.com>
2024-02-08 13:13:06 +1030

    fix: unique discovery system now works

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,9 @@ class ApiController < ApplicationController
     recipe = find_or_create_recipe
     if recipe.status_active?
       current_user.discover(recipe) if current_user.present?
-      own_discovery = (recipe.result&.discovered_uuid == params[:uuid] || recipe.result&.discovered_by == current_user)
+      own_discovery = false
+      own_discovery = recipe.result&.discovered_by == current_user if current_user.present?
+      own_discovery ||= (recipe.result&.discovered_uuid == params[:uuid]) if params[:uuid]
       return render "elements/show", locals: { element: recipe.result, own_discovery: own_discovery }
     end
 
